d1a8db10129504179c54e89d10313cca634983c3
anuragsingh013 <thakuranuragsingh099@gmail.com>
2024-03-18 17:49:29 +0530

    connect.js updaetd

--- connect.js
+++ connect.js
@@ -1,8 +1,8 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-async function connectToMongoDb(url){
-    return mongoose.connect(url);
+async function connectToMongoDb(url) {
+  return mongoose.connect(url);
 }
-module.exports={
-    connectToMongoDb
-}
+module.exports = {
+  connectToMongoDb,
+};
